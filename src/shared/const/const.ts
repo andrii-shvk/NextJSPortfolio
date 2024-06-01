c907289fb98940e8246be974d2884ce0d0b4d3d2
Andrii Shevchuk <supamercron@gmail.com>
2024-06-01 14:30:53 -0400

    add projects to projectsList

--- src/shared/const/const.ts
+++ src/shared/const/const.ts
@@ -13,51 +13,65 @@ export interface projectNamesFilter {
 
 export const projectNames: projectNamesFilter[] = [
     {name: 'All projects', dataFilter: 'all'},
-    {name: 'Front End', dataFilter: 'frontend'},
-    {name: 'Front End + Back End', dataFilter: 'backend'},
+    {name: 'React JS + Vite JS', dataFilter: 'react'},
+    {name: 'Next JS + TailwindCSS', dataFilter: 'next'},
 ];
 
 export const experienceList = [
     {
       en: {
-        title: "Winter Internship",
-        description: "Work on project with UI components, Vite.",
-        duration: "Duration: 3 month.",
-        data: "From Nov 2023 to Jan 2024",
+        title: "Capri Delivery Pizza",
+        description: "React.JS + Vite.JS, module SCSS, redux, rtkQuery, axios.",
+        duration: "Duration: 2 years and 9 months.",
+        data: "From Jan 2021 to Oct 2023",
       },
       ru: {
-        title: "Работа с кастомным проектом",
-        description: "Сайт для доставки еды в Вирджинии-Бич",
-        duration: "Длительность: 1,5 месяца.",
-        data: "От ноября 2023 до января 2024",
+        title: "Capri Доставка Пиццы",
+        description: "React.JS + Vite.JS, module SCSS, redux, rtkQuery, axios.",
+        duration: "Длительность: 2 года и 9 месяцев.",
+        data: "От января 2021 до октября 2023",
       },
     },
     {
       en: {
-        title: "Work with custom project",
-        description: "Website for delivery food in Virginia Beach.",
-        duration: "Duration: 1,5 month.",
-        data: "From Feb 2024 to March 2024",
+        title: "Kyiv Construction Company",
+        description: "Next.JS + TailwindCSS, TypeScript, routeHandler, axios, nodeMailer.",
+        duration: "Duration: 8 months.",
+        data: "From Oct 2023 to May 2024",
       },
       ru: {
-        title: "Работа с кастомным проектом",
-        description: "Сайт для доставки еды в Вирджинии-Бич",
-        duration: "Длительность: 1,5 месяца.",
-        data: "От февраля 2024 до марта 2024",
+        title: "Kyiv Construction Company",
+        description: "Next.JS + TailwindCSS, TypeScript, routeHandler, axios, nodeMailer.",
+        duration: "Длительность: 8 месяцев.",
+        data: "От октября 2023 до мая 2024",
       },
     },
     {
       en: {
-        title: "Work with custom project NextJS, TailwindCSS",
-        description: "Website for delivery food in Virginia Beach.",
-        duration: "Duration: 1,5 month.",
-        data: "From Feb 2024 to March 2024",
+        title: "My project VB-delivery React.JS + Vite.JS",
+        description: "React.JS + Vite.JS, module SCSS, redux, rtkQuery, axios.",
+        duration: "Duration: 9 month.",
+        data: "From Oct 2023 to May 2024",
       },
       ru: {
-        title: "Работа с кастомным проектом NextJS, TailwindCSS",
-        description: "Сайт для доставки еды в Вирджинии-Бич",
-        duration: "Длительность: 1,5 месяца.",
-        data: "От февраля 2024 до марта 2024",
+        title: "Мой проект VB-delivery React.JS + Vite.JS",
+        description: "React.JS + Vite.JS, module SCSS, redux, rtkQuery, axios.",
+        duration: "Длительность: 9 месяцев.",
+        data: "От октября 2023 до мая 2024",
+      },
+    },
+    {
+      en: {
+        title: "My Portoflio",
+        description: "Next.JS + TailwindCSS, TypeScript.",
+        duration: "Duration: 2 months.",
+        data: "From March 2024 to May 2024",
+      },
+      ru: {
+        title: "Мое Портфолио",
+        description: "Next.JS + TailwindCSS, TypeScript",
+        duration: "Длительность: 2 месяца.",
+        data: "От марта 2024 до мая 2024",
       },
     },
   ];
